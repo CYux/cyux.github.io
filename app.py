944ffbaef62a36e8c44758380f75022721de8042
Chan <cyux97@gmail.com>
2023-06-23 07:33:52 -0500

    Fix error to lauch

--- app.py
+++ app.py
@@ -7,10 +7,11 @@ from langchain.chains.question_answering import load_qa_chain
 from langchain.llms import OpenAI
 from langchain.callbacks import get_openai_callback
 import pickle
-
+import os
 
 def main():
     load_dotenv()
+    path = os.getcwd()
     st.set_page_config(page_title='GE semantic search')
     st.header('GE semantic search 💬')
     
@@ -19,16 +20,16 @@ def main():
 
     # Read the contents of the uploaded file
     if uploaded_file:
-        file_contents = uploaded_file.read().decode("utf-8")
+        #file_contents = uploaded_file.read().decode("utf-8")
         
         # split into chunks
-        text_splitter = CharacterTextSplitter(
-            separator='\n',
-            chunk_size=1500,
-            chunk_overlap=200,
-            length_function=len
-        )
-        chunks = text_splitter.split_text(file_contents)
+        # text_splitter = CharacterTextSplitter(
+        #     separator='\n',
+        #     chunk_size=1500,
+        #     chunk_overlap=200,
+        #     length_function=len
+        # )
+        #chunks = text_splitter.split_text(file_contents)
 
         # create embeddings
         # embeddings = OpenAIEmbeddings()
@@ -38,18 +39,21 @@ def main():
         # with open("knowledge_base.pkl", "wb") as file:
         #     pickle.dump(knowledge_base, file)
         # Load embeddings
-        with open("embeddings.pkl", "rb") as file:
+        with open(f"{path}/embeddings.pkl", "rb") as file:
             embeddings = pickle.load(file)
 
         # Load knowledge base
-        with open("knowledge_base.pkl", "rb") as file:
+        with open(f"{path}/knowledge_base.pkl", "rb") as file:
             knowledge_base = pickle.load(file)
 
         # show user input
-        user_question = st.text_input('Ask a question about your PDF:')
+        user_question = st.text_input('Keywords:')
+        k = st.number_input("Most similarity:", 
+                            min_value=1, max_value=100, step=1)
+
         if user_question:
-            docs = knowledge_base.similarity_search(user_question)
-            cnt = 0
+            docs = knowledge_base.similarity_search(user_question, k)
+            cnt = 1
             for doc in docs:
                 st.write('➡️', f'No. {cnt}', '✅', doc.page_content[201:])
                 cnt += 1
